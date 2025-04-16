0a35f64221b4ea074e76b12ff09ce609d041b866
Umayange1998 <wgikumayanga@gmail.com>
2025-04-17 02:24:21 +0530

    Employee class

--- src/main/java/com/organization/employeemanager/EmployeemanagerApplication.java
+++ src/main/java/com/organization/employeemanager/EmployeemanagerApplication.java
@@ -1,13 +1,25 @@
 package com.organization.employeemanager;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
 
 @SpringBootApplication
 public class EmployeemanagerApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+
+		System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(EmployeemanagerApplication.class, args);
+		System.out.println("DB URL from ENV: " + System.getenv("DB_URL"));
+
 	}
 
 }
---
+++ src/main/java/com/organization/employeemanager/model/Employee.java
@@ -0,0 +1,32 @@
+package com.organization.employeemanager.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+
+}
